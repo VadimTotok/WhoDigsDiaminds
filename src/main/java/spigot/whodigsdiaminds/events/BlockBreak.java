c753b049c94d3e538e4c228602ca7aac46d2e12e
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2023-05-07 10:50:06 +0300

    Update and rename PlayerInteract.java to BlockBreak.java

--- src/main/java/spigot/whodigsdiaminds/events/BlockBreak.java
+++ src/main/java/spigot/whodigsdiaminds/events/BlockBreak.java
@@ -4,25 +4,21 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 
-public class PlayerInteract implements Listener {
+public class BlockBreak implements Listener {
 
     @EventHandler
-    public void onRightClick(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            return;
-        }
-        if (event.getClickedBlock().getType() == Material.DIAMOND_ORE) {
-            Block block = event.getClickedBlock();
+    public void onBreak(BlockBreakEvent event) {
+        if (event.getBlock().getType() == Material.DIAMOND_ORE) {
+            Block block = event.getBlock();
             int x = block.getLocation().getBlockX();
             int y = block.getLocation().getBlockY();
             int z = block.getLocation().getBlockZ();
 
             event.getPlayer().getServer().getConsoleSender().sendMessage("X:" + x +"," + "Y:" + y + "," + "Z:" + z);
-        } else if (event.getClickedBlock().getType() == Material.DEEPSLATE_DIAMOND_ORE) {
-            Block block = event.getClickedBlock();
+        } else if (event.getBlock().getType() == Material.DEEPSLATE_DIAMOND_ORE) {
+            Block block = event.getBlock();
             int x = block.getLocation().getBlockX();
             int y = block.getLocation().getBlockY();
             int z = block.getLocation().getBlockZ();
