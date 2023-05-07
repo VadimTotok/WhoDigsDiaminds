2fa38c2d341949797dbbd6a4e80f699e996cb928
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2023-05-07 10:51:06 +0300

    Update WhoDigsDiaminds.java

--- src/main/java/spigot/whodigsdiaminds/WhoDigsDiaminds.java
+++ src/main/java/spigot/whodigsdiaminds/WhoDigsDiaminds.java
@@ -2,12 +2,12 @@ package spigot.whodigsdiaminds;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import spigot.whodigsdiaminds.events.PlayerInteract;
+import spigot.whodigsdiaminds.events.BlockBreak;
 
 public final class WhoDigsDiaminds extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new PlayerInteract(), this);
+        Bukkit.getPluginManager().registerEvents(new BlockBreak(), this);
     }
 }
