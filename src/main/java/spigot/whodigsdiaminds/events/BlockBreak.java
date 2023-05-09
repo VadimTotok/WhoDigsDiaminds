9a8547cf7d0fcc13c7daa0bec6d4dcba39983375
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2023-05-09 12:00:08 +0300

    Update BlockBreak.java

--- src/main/java/spigot/whodigsdiaminds/events/BlockBreak.java
+++ src/main/java/spigot/whodigsdiaminds/events/BlockBreak.java
@@ -17,24 +17,33 @@ public class BlockBreak implements Listener {
             int y = block.getLocation().getBlockY();
             int z = block.getLocation().getBlockZ();
 
-            event.getPlayer().getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[WhoDigsDiaminds] " +
-                    ChatColor.BLUE + "Material: DIAMOND_ORE " + ChatColor.RED + "X:" + x +"," + "Y:" + y + "," + "Z:" + z);
+            event.getPlayer().getServer().getConsoleSender().sendMessage(
+                    ChatColor.GREEN + "[WhoDigsDiaminds] " +
+                            ChatColor.BLUE + "Material: DIAMOND_ORE " +
+                                ChatColor.RED + "X:" + x +"," + "Y:" + y + "," + "Z:" + z
+            );
         } else if (event.getBlock().getType() == Material.DEEPSLATE_DIAMOND_ORE) {
             Block block = event.getBlock();
             int x = block.getLocation().getBlockX();
             int y = block.getLocation().getBlockY();
             int z = block.getLocation().getBlockZ();
 
-            event.getPlayer().getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[WhoDigsDiaminds] " +
-                    ChatColor.BLUE + "Material: DIAMOND_ORE " + ChatColor.RED + "X:" + x +"," + "Y:" + y + "," + "Z:" + z);
+            event.getPlayer().getServer().getConsoleSender().sendMessage(
+                    ChatColor.GREEN + "[WhoDigsDiaminds] " +
+                            ChatColor.BLUE + "Material: DIAMOND_ORE " +
+                                ChatColor.RED + "X:" + x +"," + "Y:" + y + "," + "Z:" + z
+            );
         } else if (event.getBlock().getType() == Material.ANCIENT_DEBRIS) {
             Block block = event.getBlock();
             int x = block.getLocation().getBlockX();
             int y = block.getLocation().getBlockY();
             int z = block.getLocation().getBlockZ();
 
-            event.getPlayer().getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[WhoDigsDiaminds] " +
-                    ChatColor.GOLD + "Material: ANCIENT_DEBRIS " + ChatColor.RED + "X:" + x +"," + "Y:" + y + "," + "Z:" + z);
+            event.getPlayer().getServer().getConsoleSender().sendMessage(
+                    ChatColor.GREEN + "[WhoDigsDiaminds] " +
+                            ChatColor.BLUE + "Material: ANCIENT_DEBRIS " +
+                                ChatColor.RED + "X:" + x +"," + "Y:" + y + "," + "Z:" + z
+            );
         }
     }
 }
